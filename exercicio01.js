a15296c7a15836faa808d9865a1fffb83eb1cc56
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-08 12:15:42 -0300

    melhorando o programa

--- exercicio01.js
+++ exercicio01.js
@@ -9,3 +9,29 @@ function mostra (frase){
     pulaLinha();
    
 }
+// programa inicial 
+//var idadePedro = 28;
+//var idadeMarta = 32;
+//var idadeJorge =60;
+//var idadeBete  =22;
+
+
+//var totalIdades = (idadePedro + idadeBete + idadeMarta);
+//var mediaIdades = totalIdades/4;
+
+//mostra (mediaIdades);
+
+
+
+
+// Melhorando o programa
+var idadePedro = 28;
+var idadeMarta = 32;
+var idadeJorge =60;
+var idadeBete  =22;
+
+
+var totalIdades = (idadePedro + idadeBete + idadeMarta);
+var mediaIdades = totalIdades/4;
+
+mostra (mediaIdades);
