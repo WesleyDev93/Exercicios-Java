2049fe3655333e4b5d4782d58dd503dd24c7ef16
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-08 12:38:52 -0300

    fazendo Exercicio 02

---
+++ exercicio02.js
@@ -0,0 +1,24 @@
+function pulaLinha (){
+    document.write ("<br>");
+    document.write ("<br>");
+}
+
+
+function mostra (frase){
+   document.write (frase);
+   pulaLinha();
+  
+}
+
+
+
+var numeroPensado = Math.round(Math.random()* 10);
+var chute = parseInt(prompt("Digite seu chute!"));
+
+if (chute == numeroPensado) {
+    mostra ("Voce Acertou")
+}else {
+    mostra ("voce errou o numero pensado foi " numeroPensado);
+
+}
+
